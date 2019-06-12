4ee2e985247411dcf6bb69a7366851258fed3c41
Songze <songzeli8824@gmail.com>
2019-06-12 14:59:14 -0700

    Tested coded Merkle tree construction.

--- chain/src/coded_merkle_roots.rs
+++ chain/src/coded_merkle_roots.rs
@@ -20,7 +20,7 @@ pub enum Symbols{
 	Upper(Vec<SymbolUp>),
 } 
 
-fn computeHash(coded: &Symbols) -> Vec<H256> {
+pub fn compute_hash(coded: &Symbols) -> Vec<H256> {
 	let mut roots = Vec::<H256>::new(); 
 	if let Symbols::Upper(layer) = coded {
         for i in 0..layer.len(){
@@ -52,20 +52,29 @@ fn pad(symbols: &[SymbolBase], rate: f32) -> Vec<SymbolBase> {
 pub fn encoding(data: &Symbols, rate: f32) -> Symbols {
 	match data {
 		Symbols::Base(message) => {
-			let mut coded: Vec<SymbolBase> = Vec::with_capacity(((message.len() as f32) / rate) as usize);
+			let k = message.len();
+			let n = ((k as f32) / rate) as usize;
+			let mut coded: Vec<SymbolBase> = message.clone();
+			for _i in k..n {
+				coded.push([0x00; BASE_SYMBOL_SIZE]);
+			}
+			println!("{} symbols on this level.", coded.len());
 			Symbols::Base(coded)
 		},
 		Symbols::Upper(message) => {
-			let mut coded: Vec<SymbolUp> = Vec::with_capacity(((message.len() as f32) / rate) as usize);
+			let k = message.len();
+			let n = ((k as f32) / rate) as usize;
+			let mut coded: Vec<SymbolUp> = message.clone();
+			for _i in k..n {
+				coded.push([H256::default(); AGGREGATE]);
+			}
+			println!("{} symbols on this level.", coded.len());
 			Symbols::Upper(coded)
 		},
 	}
 }
 
-fn hashAggregate(coded: &Symbols, rate: f32) -> Symbols{
-    //struct SymbolSys([H256;(AGGREGATE * rate) as u32]);
-    //struct SymbolPar([H256;(AGGREGATE * (1-rate)) as u32]);
-
+fn hash_aggregate(coded: &Symbols, rate: f32) -> Symbols{
 	let mut hashes = Vec::<H256>::new();
 	match coded {
 		Symbols::Base(message) => {
@@ -77,7 +86,7 @@ fn hashAggregate(coded: &Symbols, rate: f32) -> Symbols{
 			// for i in 0..message.len() {
 			// 	hashes.push(dhash256(&message[i]));
 			// }
-			hashes = computeHash(&coded);
+			hashes = compute_hash(&coded);
 		},
 	}
     // n is numbe of coded symbols/hashes
@@ -88,7 +97,7 @@ fn hashAggregate(coded: &Symbols, rate: f32) -> Symbols{
 	let mut new_data = Vec::with_capacity(k as usize);
 
 	for i in 0..k {
-		let mut new_symbol: SymbolUp = [H256::default();AGGREGATE];
+		let mut new_symbol: SymbolUp = [H256::default(); AGGREGATE];
 		for j in 0..(((AGGREGATE as f32) * rate) as usize){
 			let index  = ((((i * (AGGREGATE as u32)) as f32) * rate) as u32) + (j as u32);
 			new_symbol[j] = hashes[index as usize].clone();
@@ -104,10 +113,10 @@ fn hashAggregate(coded: &Symbols, rate: f32) -> Symbols{
 }
 
 /// Calculates the roots of the coded Merkle tree
-pub fn coded_merkle_roots(symbols: &[SymbolBase], headerSize: u32, rate: f32) -> (Vec<H256>, Vec<Symbols>) {
+pub fn coded_merkle_roots(symbols: &[SymbolBase], header_size: u32, rate: f32) -> (Vec<H256>, Vec<Symbols>) {
     let data = pad(symbols, rate);
     let n = ((data.len() as f32) / rate) as u32;
-    let level = ((((n/headerSize) as f32).log2()/(rate * (AGGREGATE as f32)).log2()) as u32) + 1;
+    let level = ((((n/header_size) as f32).log2()/(rate * (AGGREGATE as f32)).log2()) as u32) + 1;
 
     //Coded merkle tree is a vector of symbols on each layer
     let mut tree: Vec<Symbols> = Vec::with_capacity(level as usize); 
@@ -117,44 +126,44 @@ pub fn coded_merkle_roots(symbols: &[SymbolBase], headerSize: u32, rate: f32) ->
 
     // Construct upper layers
     for i in 0..(level-1) {
-    	let new_data: Symbols = hashAggregate(&tree[i as usize], rate);
+    	let new_data: Symbols = hash_aggregate(&tree[i as usize], rate);
     	tree.push(encoding(&new_data, rate));
     }
-    (computeHash(&tree[tree.len()-1]), tree)
+    (compute_hash(&tree[tree.len()-1]), tree)
 }
-
 
 #[cfg(test)]
 mod tests {
 	use hash::H256;
-	use super::coded_merkle_roots;
+	use super::*;
 
-	// block 80_000
-	// https://blockchain.info/block/000000000043a8c0fd1d6f726790caa2a406010d19efd2780db27bdbbd93baf6
+	//Test for construction of a coded Merkle tree
 	#[test]
-	fn test_coded_merkle_roots_with_2_hashes() {
-		let tx1 = H256::from_reversed_str("c06fbab289f723c6261d3030ddb6be121f7d2508d77862bb1e484f5cd7f92b25");
-		let tx2 = H256::from_reversed_str("5a4ebf66822b0b2d56bd9dc64ece0bc38ee7844a23ff1d7320a88c5fdb2ad3e2");
-		let expected = H256::from_reversed_str("8fb300e3fdb6f30a4c67233b997f99fdd518b968b9a3fd65857bfe78b2600719");
+	fn test_coded_merkle_roots1() {
+		let symbols = [[0x0f; BASE_SYMBOL_SIZE]; 200]; //The tree has 200 symbols on the base layer
+		let roots = Vec::<H256>::new();
+		let tree = Vec::<Symbols>::new();
+		//Construct a coded merkle tree with 16 coded symbols on the top layer 
+		//Coding rate is 0.25, and hence the tree has 7 layers  
+		let (roots, tree) = coded_merkle_roots(&symbols, 16, 0.25);  
 
-		let result = merkle_root(&[&tx1, &tx2]);
-		let result2 = merkle_root(&[tx1, tx2]);
-		assert_eq!(result, expected);
-		assert_eq!(result2, expected);
+		assert_eq!(roots.len(), 16);
+		assert_eq!(tree.len(), 7);
+		assert_eq!(compute_hash(&tree[tree.len() - 1]), roots); 
 	}
 
-	// Test with 5 hashes
+	//Test for construction of another coded Merkle tree
 	#[test]
-	fn test_coded_merkle_roots_with_5_hashes() {
-		let mut vec = Vec::new();
-		vec.push(H256::from_reversed_str("1da63abbc8cc611334a753c4c31de14d19839c65b2b284202eaf3165861fb58d"));
-		vec.push(H256::from_reversed_str("26c6a6f18d13d2f0787c1c0f3c5e23cf5bc8b3de685dd1923ae99f44c5341c0c"));
-		vec.push(H256::from_reversed_str("513507fa209db823541caf7b9742bb9999b4a399cf604ba8da7037f3acced649"));
-		vec.push(H256::from_reversed_str("6bf5d2e02b8432d825c5dff692d435b6c5f685d94efa6b3d8fb818f2ecdcfb66"));
-		vec.push(H256::from_reversed_str("8a5ad423bc54fb7c76718371fd5a73b8c42bf27beaf2ad448761b13bcafb8895"));
-		let result = merkle_root(&vec);
+	fn test_coded_merkle_roots2() {
+		let symbols = [[0x0f; BASE_SYMBOL_SIZE]; 1000]; //The tree has 1000 symbols on the base layer
+		let roots = Vec::<H256>::new();
+		let tree = Vec::<Symbols>::new();
+		//Construct a coded merkle tree with 4 coded symbols on the top layer 
+		//Coding rate is 0.5, and hence the tree has 6 layers  
+		let (roots, tree) = coded_merkle_roots(&symbols, 4, 0.5);  
 
-		let expected = H256::from_reversed_str("3a432cd416ea05b1be4ec1e72d7952d08670eaa5505b6794a186ddb253aa62e6");
-		assert_eq!(result, expected);
+		assert_eq!(roots.len(), 4);
+		assert_eq!(tree.len(), 6);
+		assert_eq!(compute_hash(&tree[tree.len() - 1]), roots); 
 	}
 }
