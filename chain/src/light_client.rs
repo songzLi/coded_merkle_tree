8024a8552fdb306585adaa7bf6bfdca61711f7ff
Songze <songzeli8824@gmail.com>
2019-07-03 16:18:02 -0700

    Updated sampling function at light nodes.

--- chain/src/light_client.rs
+++ chain/src/light_client.rs
@@ -44,24 +44,51 @@ impl LightClient {
 
 		for _ in 1..s {
 			let throw = die.sample(&mut rng);
-			if !index.contains(&throw) {
-				let (symbol, proof): (Option<SymbolBase>, Option<Vec<SymbolUp>>) = 
-				match (symbol, proof) {
-					(None, _) => panic!("Symbol No.{} is missing!", throw),
-					(_, None) => panic!("Merkle proof for symbol No.{} is missing!", throw),
-					(Some(s), Some(p)) => {
-						if self.block_header.verify_base(s, throw, k, &p) {
-					        index.push(throw);
-					        symbols.push(s);
-					        proofs.push(p);
-					    } else {
-					        panic!("Merkle proof for symbol No.{} does not pass!", throw);
-					    }
-					},
-				}
+			//if !index.contains(&throw) {
+			let (symbol, proof): (Option<SymbolBase>, Option<Vec<SymbolUp>>) = 
+			match (symbol, proof) {
+				(None, _) => panic!("Symbol No.{} is missing!", throw),
+				(_, None) => panic!("Merkle proof for symbol No.{} is missing!", throw),
+				(Some(s), Some(p)) => {
+					if self.block_header.verify_base(s, throw, k, &p) {
+					    index.push(throw);
+					    symbols.push(s);
+					    proofs.push(p);
+					} else {
+					    panic!("Merkle proof for symbol No.{} does not pass!", throw);
+					}
+				},
 			}
+			//}
 		}
 
 		(index, symbols, proofs)
 	}
+
+    //Take s samples from each upper layer of coded Merkle tree
+	pub fn sampling_upper(&self, index: Vec<u64>, proofs: Vec<Vec<SymbolUp>>, s: u32) -> (Vec<Vec<u64>>, 
+		Vec<Vec<SymbolUp>>, Vec<Vec<Option<Vec<SymbolUp>>>>) {
+		let height = proofs[0].len() + 1; //height of coded Merkle tree
+		for j in 0..s { //iterate over s samples
+			for i in 1..height {  //iterate over height - 1 upper layers
+				let mut symbol: SymbolUp = proofs[j][i - 1];
+				if i < height - 1 {
+				    let mut proof: Option<Vec<SymbolUp>> = Some(proofs[j][i..(height - 1)].to_vec());
+				} else {
+					let mut proof: Option<Vec<SymbolUp>> = None;
+				}
+	
+
+			} 
+
+		}
+
+
+	}
+
+
+
+
+
+
 }
